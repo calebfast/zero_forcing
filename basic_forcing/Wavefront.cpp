1f6a554809f6d8f468d7b02c654632015ce3fb71
Caleb Fast <calebfast@gmail.com>
2017-03-29 00:07:04 -0500

    Adding basic repo structure

---
+++ basic_forcing/Wavefront.cpp
@@ -0,0 +1,9 @@
+/**********************************
+This program is a C++ implementation of the Wavefront Algorithm for finding 
+minimum zero forcing sets of a graph.  
+
+Author: Caleb Fast
+Date: 2017
+
+Usage: ./Wavefront <path to .edg file>
+**********************************/
---
+++ connected_forcing/branch_and_bound.cpp
@@ -0,0 +1,11 @@
+/*********************
+
+This program uses a branch and bound style search together with a method for enumerating all connected subgraphs of a graph to find a minimum connected zero forcing set of the graph.
+
+Author: Caleb Fast
+Year: 2017
+
+Usage: ./branch_and_bound <path to .edg file>
+
+ ********************/
+
